e182df2a75dbe14244e60c8492e845b415c44176
xharix <harigerza@gmail.com>
2021-06-24 00:50:10 +0200

    Anpasungen im Background
    Versuche einzelne DIV-Farben umzustellen

--- Backgroundchange.js
+++ Backgroundchange.js
@@ -5,17 +5,20 @@ function updateBackground() {
     var 
       hr = (new Date()).getHours(), //aktuelle Stunde holen
       min = (new Date()).getMinutes(), //aktuelle Minuten holen
-      body = document.body,
-      bstyle = body.style;
+      body = document.body, //gesamten Body auswaehlen
+      links = body.getElementsByClassName("links"), //links-Block im Body auswaehlen
+      cta = body.getElementsByClassName("cta"); //cta-Button im Body auswaehlen
+      bstyle = body.style; //gesamten Body-style auswaehlen
 
 console.log(hr+":"+min); //Zeit in COnsole Ausgeben, nur zur Ueberpruefung
+console.log(links);
 
-    if (hr >=20) { //Falls nach 20:00 Uhr Abends
-      bstyle.backgroundColor = "black";
-      bstyle.color = "white";
-    } else if (hr <= 08) { //Falls vor 08:00 Uhr Morgens
-      bstyle.backgroundColor = "black";
-      bstyle.color = "white";
+    if (hr >=20 || hr<=08) { //Falls nach 20:00 Uhr Abends oder vor 08:00 Uhr Morgens
+      bstyle.backgroundColor = "black"; //Hintergrundfarbe des Body
+      //links.style.backgroundColor = "blue"; //Hintergrundfarbe des Links-Blocks
+      links.backgroundColor = "blue";
+      cta.backgroundColor = "red";
+      bstyle.color = "black"; //Textfarbe
     } else {
       bstyle.backgroundColor = "Green";
       bstyle.color = "white";
